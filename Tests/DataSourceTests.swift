5da0deac917d9c44b08598bec1f1248ac1c8deb7
Daniel Thorpe <danthorpe@me.com>
2016-03-27 14:41:42 +0100

    [TAY-44]: Renames Datasource to DataSource

---
+++ Tests/DataSourceTests.swift
@@ -0,0 +1,23 @@
+//
+//  DataSourceTests.swift
+//  TaylorSource
+//
+//  Created by Daniel Thorpe on 11/03/2016.
+//
+//
+
+import XCTest
+
+class DataSourceTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+
+    }
+
+    override func tearDown() {
+
+        super.tearDown()
+    }
+
+}
