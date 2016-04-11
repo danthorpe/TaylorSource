9a9aebb9c348b1620a2d14b90d5433c563efd3f2
Daniel Thorpe <danthorpe@me.com>
2016-04-11 07:11:04 +0100

    [TAY-44]: Starts tests for FetchedResultsController

---
+++ Tests/FetchedResultsDataSourceTests.swift
@@ -0,0 +1,11 @@
+//
+//  FetchedResultsDataSourceTests.swift
+//  TaylorSource
+//
+//  Created by Daniel Thorpe on 11/04/2016.
+//  Copyright © 2016 Daniel Thorpe. All rights reserved.
+//
+
+import Foundation
+import CoreData
+@testable import TaylorSource
